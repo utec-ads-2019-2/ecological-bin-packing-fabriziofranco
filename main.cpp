6c513b39a6c4efc5b283219206108ba5b4cbc205
Fabrizio David Franco Amayo <39867790+fabriziofranco@users.noreply.github.com>
2019-08-24 23:01:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int hallar_movimientos_por_color(int b, int c){
-    return b+c;
-}
-
 int hallar_movimientos_por_combinacion(int a,int b,int c,int d,int e ,int f){
-    return hallar_movimientos_por_color(a,b)+hallar_movimientos_por_color(c,d)+hallar_movimientos_por_color(e,f);
+    return a+b+c+d+e+f;
 }
 
 void halla_numero_minimo_de_6(int num1, int num2,int num3,int num4,int num5,int num6){
@@ -39,5 +35,4 @@ int main() {
     while(cin>> bin1_b>>bin1_g>>bin1_c>>bin2_b>>bin2_g>>bin2_c>>bin3_b>>bin3_g>>bin3_c)
         hallar_minima_combinacion_int(bin1_b,bin1_g,bin1_c,bin2_b,bin2_g,bin2_c,bin3_b,bin3_g,bin3_c);
     return 0;
-
 }
